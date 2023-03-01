89a1693c0c2a922c9a048c6f147c92000fb47ddc
alisonViana <alisonviana.c@gmail.com>
2023-03-01 03:30:38 -0300

    feat: create html page to show all students

--- src/main/java/br/com/alisonviana/controller/StudentController.java
+++ src/main/java/br/com/alisonviana/controller/StudentController.java
@@ -1,5 +1,6 @@
 package br.com.alisonviana.controller;
 
+import br.com.alisonviana.entity.Student;
 import br.com.alisonviana.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,7 +15,7 @@ public class StudentController {
 
     @GetMapping("/students")
     public String students(Model model) {
-        model.addAttribute("students", studentService.getAllStudents());
+        model.addAttribute("k_students", studentService.getAllStudents());
 
         return "students";
     }
