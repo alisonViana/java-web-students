972c8ee17779ce430a9eb4a07e5d1187e58ee115
alisonViana <alisonviana.c@gmail.com>
2023-03-01 04:25:44 -0300

    feat: implements add new student

--- src/main/java/br/com/alisonviana/controller/StudentController.java
+++ src/main/java/br/com/alisonviana/controller/StudentController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -18,5 +20,20 @@ public class StudentController {
         model.addAttribute("k_students", studentService.getAllStudents());
 
         return "students";
+    }
+
+    @GetMapping("/students/new")
+    public String createStudentForm(Model model) {
+        Student student = new Student();
+        model.addAttribute("k_student", student);
+
+        return "new_student";
+    }
+
+    @PostMapping("/new_student")
+    public String saveStudent(@ModelAttribute("k_student") Student student) {
+        studentService.saveStudent(student);
+
+        return "redirect:/students";
     }
 }
--- src/main/java/br/com/alisonviana/service/impl/StudentServiceImpl.java
+++ src/main/java/br/com/alisonviana/service/impl/StudentServiceImpl.java
@@ -19,4 +19,9 @@ public class StudentServiceImpl implements StudentService {
         return studentRepository.findAll();
     }
 
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
 }
